fa986eb48249fed7aa6bcb76426d2b69d53a35a1
Davey <daveybelliss@gmail.com>
2019-02-25 02:16:24 -0800

    Able to scale, translate, and rotate using vertex shader. Still issues with rotation when object is not at origin. Working on loading external model

--- flyingCube.js
+++ flyingCube.js
@@ -1,7 +1,10 @@
 "use strict";
 
 function Initialize() {
-    var game = new Game();
+
+    loadJSONResource('./Assets/Asteroid.json', function (modelErr, modelObj) {
+        var game = new Game(modelObj);
+    });
 }
 
 class Game {
@@ -11,7 +14,7 @@ class Game {
         this.projMatrix = new Float32Array(16);
     }
 
-    constructor() {
+    constructor(asteroidJson) {
         // Create WebGL object
         var canvas = document.getElementById('game-surface');
         this.canvas = canvas;
@@ -61,8 +64,8 @@ class Game {
         this.crates = []
         this.asteroids = []
         this.createPlayer();
-        this.createAsteroids(1)
-        this.createCrates(0)
+//        this.createAsteroids(20, asteroidJson)
+        this.createCrates(100)
         this.createWalls()
         this.camera = new Camera(gl, this.worldMatrix, this.viewMatrix, this.projMatrix);
 
@@ -77,13 +80,26 @@ class Game {
             theta = cameraInput.x;
             phi = cameraInput.y;
             this.camera.trackObject(this.player, theta, phi);
-                        this.textureProgram.updateCamera()
-            this.renderer.render()
+            this.textureProgram.updateCamera()
+            for (var i = 0; i < this.crates.length; i++) {
+                var gameObject = this.crates[i]
+                var renderData = gameObject.renderData;
+                renderData.program.draw(this.gl, renderData.vertices, renderData.indices, renderData.texture, renderData.drawType, gameObject.transform)
+            }
+
+            for (var i = 0; i < this.asteroids.length; i++) {
+                var gameObject = this.asteroids[i]
+                var jsonData = gameObject.json;
+                this.textureProgram.drawMesh(this.gl, asteroidJson.meshes[0].vertices, [].concat.apply([], asteroidJson.meshes[0].faces), asteroidJson.meshes[0].texturecoords[0], this.textureLoader.textures["default"], gl.DYNAMIC_DRAW, gameObject.transform)
+            }
+
+            var renderData = this.player.renderData;
+            renderData.program.draw(this.gl, renderData.vertices, renderData.indices, renderData.texture, renderData.drawType, this.player.transform)
+
             numFrames++;
             requestAnimationFrame(render.bind(this));
         };
         requestAnimationFrame(render.bind(this));
-
         this.beginFixedUpdateLoop();
     }
 
@@ -104,26 +120,49 @@ class Game {
                 gameObject.fixedUpdate(.02)
             })
 
-            if (updateNum % 100 == 0) {
-                for (var i = 0; i < this.crates.length; i++) {
-                    this.crates[i].addForce(Vector3.random(10));
+            this.player.transform.rotation.x = (this.player.transform.rotation.x + 1) % 360
+            console.log(this.player.transform.position)
+            for (var i = 0; i < this.crates.length; i++) {
+                var crate = this.crates[i];
+                if (updateNum % 1000 == 60) {
+//                    this.crates[i].transform.position = new Vector3(0,0,0)
                 }
+                if (i < this.crates.length/3) {
+                    crate.transform.rotation.x = (crate.transform.rotation.x + 1) % 360
+                }
+                else if (i < this.crates.length * 2/3) {
+                    crate.transform.rotation.y = (crate.transform.rotation.y + 1) % 360
+                }
+                else{
+                    crate.transform.rotation.z = (crate.transform.rotation.z + 1) % 360
+                }
+
+
             }
 
-            if (updateNum % 100 == 50) {
-                for (var i = 0; i < this.asteroids.length; i++) {
-                    this.asteroids[i].addForce(Vector3.random(10));
-                }
-            }
+//            if (updateNum % 100 == 0) {
+
+//            }
+//
+//            if (updateNum % 100 == 50) {
+//                for (var i = 0; i < this.asteroids.length; i++) {
+//                    this.asteroids[i].addForce(Vector3.random(10));
+//                }
+//            }
             updateNum++;
             await this.sleep(1000/60);
         }
     }
 
-    createAsteroids(numAsteroids) {
+    createAsteroids(numAsteroids, asteroidJson) {
         console.log("Creating asteroids");
         for (var i = 0; i < numAsteroids; i++) {
-            var asteroid = new Cube("asteroid" + i, Vector3.random(30), "susan", this.gl.DYNAMIC_DRAW)
+            var asteroid = new MeshObject("asteroid" + i, Vector3.random(30), this.textureLoader.getTexture("susan"), this.gl.DYNAMIC_DRAW, this.textureProgram)
+            asteroid.transform.scale.add(Vector3.random(10))
+            asteroid.transform.rotation.x = Math.random() * 360
+            asteroid.transform.rotation.y = Math.random() * 360
+            asteroid.transform.rotation.z = Math.random() * 360
+            asteroid.transform.scale = new Vector3(.1,.1,.1)
             this.asteroids.push(asteroid);
             this.addGameObject(asteroid);
         }
@@ -131,14 +170,17 @@ class Game {
 
     createPlayer() {
         console.log("Creating player");
-        this.player = new Player("Player", new Vector3(0,0,0), "burningCrate", this.gl.DYNAMIC_DRAW);
+        this.player = new Player("Player", new Vector3(0,0,0), this.textureLoader.getTexture("burningCrate"), this.gl.DYNAMIC_DRAW, this.textureProgram);
         this.addGameObject(this.player);
     }
 
     createCrates(numCrates) {
         console.log("Creating crates");
         for (var i = 0; i < numCrates; i++) {
-            var crate = new Cube("crate" + i, Vector3.random(20), "crate", this.gl.DYNAMIC_DRAW)
+            var crate = new Cube("crate" + i, Vector3.random(10), this.textureLoader.getTexture("crate"), this.gl.DYNAMIC_DRAW, this.textureProgram)
+            crate.transform.rotation.x = Math.random() * 360
+            crate.transform.rotation.y = Math.random() * 360
+            crate.transform.rotation.z = Math.random() * 360
             this.crates.push(crate);
             this.addGameObject(crate);
         }
